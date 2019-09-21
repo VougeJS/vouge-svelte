7016c8cdcdd1a584e07a0e1e7f876bfec973d47b
Savander <savander.pl@gmail.com>
2019-09-22 01:52:15 +0200

    feat(init): VButton Component

---
+++ src/components/button/VButton.svelte
@@ -0,0 +1,31 @@
+<script>
+    /**
+     * @type { ('primary', 'secondary', 'success') }
+     */
+    export let variant;
+
+    $: classNames = (() => {
+        let className = 'btn';
+
+        if (variant) {
+            className += ` ${ variant }`;
+        }
+
+        return className;
+    })();
+</script>
+
+<style lang="scss">
+    .btn {
+        display: inline-flex;
+        font-weight: 400;
+        align-content: center;
+        justify-content: center;
+        text-align: center;
+        user-select: none;
+    }
+</style>
+
+<button class="{classNames}">
+    <slot></slot>
+</button>
